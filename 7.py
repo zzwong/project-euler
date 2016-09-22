cda15ab418d5968cc493a6be8a3caa6ea43b5a67
Aaron Wong <aaronwong13@gmail.com>
2016-09-22 13:37:31 -0700

    added description to 7

---
+++ 7.py
@@ -0,0 +1,20 @@
+'''
+10001'st prime
+
+
+First 6 primes are: 2,3,4,5,7,11, 13
+'''
+
+
+'''
+Idea 1:
+Loop through 1,000,000 numbers and check if it is prime
+	add primes to a list of primes
+	if len(list_primes) >=10,001
+		break
+return the last element in list_primes
+
+Idea 2:
+Write a function that given a number n, returns the nth prime. 
+print nthPrime(10001)
+'''
